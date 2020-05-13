d43df762624757762e322e0fe733edffc560ea3d
Angela Bumgarner <amrvitale@gmail.com>
2020-05-13 13:18:13 -0700

    added shopping list js and spec.js

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,15 @@
+INSERT INTO blogful_articles 
+    (title, content, date_published)
+VALUES
+('Drinking coffee with Derek', 'Angela and Derek drink at Press Coffee in Phoenix before Coronavirus.', now() - '78 days'::INTERVAL),
+('Exhaustion and the coronavirus', 'Tired of not going out? Get used to it.', now() - '13 days'::INTERVAL),
+('Dogs are our best friends','They are never sick of you even though you have not left the house in days', now() - '3 days'::INTERVAL),
+('Swedish Food for Dummies','Easy recipes for all!', now() - '24 days'::INTERVAL),
+('Fold your socks better','On choosing your domestic battles', now() - '11 days'::INTERVAL),
+('The new iPhone is not so bad','And 0.7 inches larger!', now() - '6 days'::INTERVAL),
+('My vacation was canceled','The German trip I never got to have', now() - '38 days'::INTERVAL),
+('Coronavirus sucks','Because, everything.', now() - '1 days'::INTERVAL),
+('Halloween is my favorite holiday','Coronavirus will also make it lame.', now() - '31 days'::INTERVAL),
+('So darn tired','Try to keep awake', now() - '8 days'::INTERVAL),
+('I copy paste in my sleep','Here is why!', now() - '42 days'::INTERVAL)
+;
